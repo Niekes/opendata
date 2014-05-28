356024fb45eb8702a2dd52659321d55f6de03b24
Michél Neumann <neumann.michel93@gmail.com>
2014-05-28 15:11:53 +0200

    Map positioning on select

--- js/interface.js
+++ js/interface.js
@@ -1,6 +1,7 @@
 var userPos;
-const initLat = 52.523;
-const initLng = 13.411;
+var map;
+var lats = [52.53064, 52.51592, 52.45000, 52.55000, 52.53330, 52.51670, 52.53611];
+var lngs = [13.38307, 13.45457, 13.56670, 13.55000, 13.16670, 13.30000, 13.60497];
 
 //When document is ready
 $(function()
@@ -10,11 +11,27 @@ $(function()
 		navigator.geolocation.getCurrentPosition(function(position) {
             userPos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
             	//Load a new instance of google map
-			var map = new google.maps.Map(document.getElementById("map"), {
-				zoom: 12,
+			map = new google.maps.Map(document.getElementById("map"), {
+				zoom: 13,
 				center: userPos,
 				disableDefaultUI: true
 			});
+
+			new google.maps.Marker({
+				position: userPos,
+				map: map,
+				title: "Your position"
+			})
 		});
 	}
 });
+
+function centerMap(obj) {
+	var options = $(obj).find("option");
+	for(var i = 0; i < options.length; i++) {
+		if(options[i].selected) {
+			map.panTo(new google.maps.LatLng(lats[i], lngs[i]));
+			break;
+		}
+	}
+}
